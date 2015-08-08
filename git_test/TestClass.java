6f9f965b323277051ce59989d94a65afa7e28c15
iliasgr <ilias.skordilis@gmail.com>
2015-08-08 09:13:00 -0400

    calling child

--- git_test/TestClass.java
+++ git_test/TestClass.java
@@ -1,9 +1,16 @@
 class TestClass {
+	TestClassChild ch;
 	
 	public TestClasss(){
 		
+		ch = new TestClassChild();
+		
+		
 	}
 	public static void main(String [] args) {
+		TestClass tc = new TestClass();
+		tc.ch.Show("Hello Helper class with Interface");
+		
 		System.out.println("Hi! Testing git");
 	}
 
